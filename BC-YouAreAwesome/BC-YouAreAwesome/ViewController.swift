c96ffc44e581566a010329cc9f4cc48650f26f1d
jjb-rbs <111300320+dev-royalbluesoftware@users.noreply.github.com>
2023-12-28 17:56:37 -0700

    app complete programmatic

--- BC-YouAreAwesome/BC-YouAreAwesome/ViewController.swift
+++ BC-YouAreAwesome/BC-YouAreAwesome/ViewController.swift
@@ -5,17 +5,58 @@
 //
 // Created by jjbuell-dev
 // Royal Blue Software
-// 
+//
 
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    // MARK: - Outlets
+ 
+    // MARK: - UI Properties
     
-    @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var imageView0: UIImageView!
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 31, weight: .bold)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    let horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 38
+        return stackView
+    }()
+    
+    let showMessageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.systemIndigo, for: .normal)
+        button.setTitle("Show Message", for: .normal)
+        
+        return button
+    }()
+    
+    let showAnotherMessageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.systemIndigo, for: .normal)
+        button.setTitle("Show Another Message", for: .normal)
+        
+        return button
+    }()
+    
+    let imageView0: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
     
     // MARK: - Properties
     
@@ -27,20 +68,52 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
         messageLabel.isHidden = true
+        setupViews()
+        buttonTargetActions()
     }
     
-    // MARK: - Actions
+    // MARK: - Helper Functions
     
-    @IBAction func showMessageButtonPressed(_ sender: UIButton) {
+    private func buttonTargetActions() {
+        showMessageButton.addTarget(self, action: #selector(showMessageButtonPressed), for: .touchUpInside)
+        showAnotherMessageButton.addTarget(self, action: #selector(showAnotherMessageButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc func showMessageButtonPressed() {
         messageLabel.isHidden = false
         messageLabel.text = messagesArray.randomElement()
         imageView0.image = imagesArray.randomElement()!
     }
     
-    @IBAction func showAnotherMessageButtonPressed(_ sender: UIButton) {
+    @objc func showAnotherMessageButtonPressed() {
         messageLabel.isHidden = false
         messageLabel.text = messagesArray.randomElement()
         imageView0.image = imagesArray.randomElement()!
+    }
+    
+    // MARK: - UI Functions
+    
+    private func setupViews() {
+        view.addSubview(messageLabel)
+        view.addSubview(horizontalStackView)
+        horizontalStackView.addArrangedSubview(showMessageButton)
+        horizontalStackView.addArrangedSubview(showAnotherMessageButton)
+        view.addSubview(imageView0)
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            messageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            horizontalStackView.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 48),
+            horizontalStackView.centerXAnchor.constraint(equalTo: messageLabel.centerXAnchor),
+            
+            imageView0.topAnchor.constraint(equalTo: horizontalStackView.bottomAnchor, constant: 16),
+            imageView0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            imageView0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            imageView0.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -24),
+        ])
     }
 }
