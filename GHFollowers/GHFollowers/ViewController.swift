361d087ce7afc9d0027bf3b369eaf2e42359b7af
jjb-rbs <111300320+dev-royalbluesoftware@users.noreply.github.com>
2024-01-04 13:53:51 -0700

    Add GHFollowers - initial setup

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+// GHFollowers
+// ViewController.swift
+//
+// Created by jjbuell-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
